eaa3232097fe279e0a8d05508267b7cd9277d944
liheng_xiaomi <375461826@qq.com>
2023-06-29 12:41:23 +0800

    1

--- 002.人工智能/worspace/month04/day01/01_ndarray_2.py
+++ 002.人工智能/worspace/month04/day01/01_ndarray_2.py
@@ -13,13 +13,22 @@ ary = np.arange(0.1, 1.2, 0.1)
 print(ary)
 
 
+
+
+
 print("==" * 20)
 pi = math.pi
 print(pi)
 print(-pi)
 
+
 print("==" * 20)
-np.
-ary = np.arange(-pi,pi,)
+#（1）先求步长
+setp = pi * 2 / 200
+#（2）再通过arange接口求出等差数列
+ary = np.arange(-pi, pi, setp)
+print(ary)
 
-
+print("==" * 20)
+ary = np.linspace(-np.pi, np.pi, 200)
+print(ary)
