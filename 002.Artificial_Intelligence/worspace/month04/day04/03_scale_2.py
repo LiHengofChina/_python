d8d355aac623e0487e2d3599f7d0949b1e065207
liheng_xiaomi <375461826@qq.com>
2023-07-12 00:24:04 +0800

    11

---
+++ 002.Artificial_Intelligence/worspace/month04/day04/03_scale_2.py
@@ -0,0 +1,17 @@
+'''
+标准化（均值移除）
+
+ 使用 sklearn 框架来实现
+
+'''
+import numpy as np
+import sklearn.preprocessing as sp
+
+# 构建一个3行3列的数据，人工智能里面数据一般都是浮点数
+raw_sample = np.array([[3.0,-100.0,2000.0],
+                       [0.0,400.0,3000.0],
+                       [1.0,-400.0,2000.0]])
+
+#使用 sklearn 框架来实现
+res = sp.scale(raw_sample)
+print(res)
