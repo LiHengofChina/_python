9d512713983f581f9c76a3814623e94d477f8e36
liheng_xiaomi <375461826@qq.com>
2023-07-13 11:58:02 +0800

    111

--- 002.Artificial_Intelligence/worspace/month04/day04/08_labelencoder.py
+++ 002.Artificial_Intelligence/worspace/month04/day04/08_labelencoder.py
@@ -7,9 +7,16 @@ import numpy as np
 import sklearn.preprocessing as sp
 
 #标签编码只能转换一维数据
-data = np.array(['bmw','byd','bmw','benz','byd','audi'])
 
 
+raw_samples = np.array(['bmw','BYD','bmw',
+						'benz','BYD','audi'])
 
+lb_encoder = sp.LabelEncoder() #定义标签编码对象
+lb_samples = lb_encoder.fit_transform(raw_samples) #执行标签编码#训练并转换
+print(lb_samples)
+
+
+print(lb_encoder.inverse_transform(lb_samples)) #逆向转换
 
 
