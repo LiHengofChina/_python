ffac8b849e9a7e09e79471c35caf0ec66fe86321
liheng <liheng@liheng.liheng>
2026-08-18 17:30:30 +0800

    OK

--- 002.Artificial_Intelligence/011.nlp/workspace_011.nlp/day_26_3/01_pos_demo.py
+++ 002.Artificial_Intelligence/011.nlp/workspace_011.nlp/day_26_3/01_pos_demo.py
@@ -10,8 +10,9 @@ jieba
 def pos(text):
     results = psg.cut(text)  # 分词，记性标注
 
-    for w, t in results:
-        print("%s/%s" % (w, t), end=" ")
+    # pair 对象不能解包，用 word/flag 取词和词性
+    for item in results:
+        print("%s/%s" % (item.word, item.flag), end=" ")
     print("")
 
 text = "梅兰芳大剧院周六晚上有演出"
